eea680842183cf1768c5272016145e33cf382723
KompotMaster28 <peterpeev28@gmail.com>
2020-02-27 18:59:40 +0200

    Merging

--- Folder1/plamena.ts
+++ Folder1/plamena.ts
@@ -1,10 +1,7 @@
 export class Model {
     a: number;
     plamito: string;
-<<<<<<< HEAD
     neshto: number;
-=======
     plamito2: string;
->>>>>>> c7a7c45b5a59d6e5249e7cb001317d9340ff00e5
     c: string[];
 }
